1abd99cdaebced7a3bd4c6d39344df3cdfe07474
parksuyeonn <clapyyyyy@gmail.com>
2023-04-25 17:02:06 +0900

    api prac

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
-const http = require('http');
-const fs = require("fs");
+// const http = require('http');
+// const fs = require("fs");
+import http from "http"
+import fs from "fs"
+// import locationMap from "./locationMap.js"
+// import currentLocation from './geolocation.js'
 
-const server = http.createServer((req, res) => {
+//여기에 경로
+// const server = http.createServer(function(req, res){
+//   if(req.method === "GET"){
+
+//   }
+//   if(req.method === "POST"){
+
+//   }
+// })
+
+http.createServer((req, res) => {
+  // var url = req.url;
+  // if(req.url === '/'){
+  //   url = '/map.html';
+  // }
+  // if(request.url == '/favicon.ico'){
+  //   response.writeHead(404);
+  //   response.end();
+  //   return;
+  // }
+  // response.writeHead(200);
+  // response.end(fs.readFileSync(__dirname + url));
   fs.readFile('map.html',function(err, data){
   res.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
   res.write(data);
