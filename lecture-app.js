d8eaf32b3e85bb4298d83f972848a37a1f6defa0
kongukjae <kongukjae@gmail.com>
2023-04-27 14:36:40 +0900

    lecture-app, 팀원 작업 기록

---
+++ lecture-app.js
@@ -0,0 +1,25 @@
+import http from "http"
+import fs from "fs"
+import url from "url"
+
+const server = http.createServer(function(request, response) {
+  const parsedUrl = url.parse(request.url);
+  const resource = parsedUrl.pathname;
+
+  if(resource === "/0427main") {
+    fs.readFile("./0427main.html", 'utf-8', function(error, data) {
+      if(error) {
+        response.writeHead(500, {'Content-Type':'text/html'});
+        response.end('500 Internal Server '+error);
+      } else {
+        response.writeHead(200, {'Content-Type':'text/html'});
+        response.end(data);
+      }
+
+    });
+  }
+});
+
+server.listen(8080, function(){
+  console.log('Server is running...8080');
+});
