2c4c26af18ec2aebc75384e7eeb67deaefa1bf4c
kongukjae <kongukjae@gmail.com>
2023-04-27 14:41:41 +0900

    변수 찾아냈어!

--- lecture-app.js
+++ lecture-app.js
@@ -6,18 +6,21 @@ const server = http.createServer(function(request, response) {
   const parsedUrl = url.parse(request.url);
   const resource = parsedUrl.pathname;
 
-  if(resource === "/0427main") {
-    fs.readFile("./0427main.html", 'utf-8', function(error, data) {
-      if(error) {
-        response.writeHead(500, {'Content-Type':'text/html'});
-        response.end('500 Internal Server '+error);
-      } else {
-        response.writeHead(200, {'Content-Type':'text/html'});
-        response.end(data);
-      }
+  console.log("이거 URL이여?", parsedUrl);
+  console.log("이게 리소스여?", resource);
 
-    });
-  }
+  // if(resource === "/0427main") {
+  //   fs.readFile("./0427main.html", 'utf-8', function(error, data) {
+  //     if(error) {
+  //       response.writeHead(500, {'Content-Type':'text/html'});
+  //       response.end('500 Internal Server '+error);
+  //     } else {
+  //       response.writeHead(200, {'Content-Type':'text/html'});
+  //       response.end(data);
+  //     }
+
+  //   });
+  // }
 });
 
 server.listen(8080, function(){
